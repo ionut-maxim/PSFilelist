b8efdfe6fa8272ab920299d07e2fc1aaf8b0f9e2
Ionuț Maxim <Ionu Maxim>
2017-01-21 14:38:34 +0200

    Added appveyor.yml for testing

---
+++ Tests/PSFilelist.Tests.ps1
@@ -0,0 +1,18 @@
+﻿#requires -Version 3
+#Variables for Pester tests
+
+$ModulePath = Split-Path -Parent -Path (Split-Path -Parent -Path $MyInvocation.MyCommand.Path)
+$ModuleName = 'PSFilelist'
+$ManifestPath   = "$ModulePath\PSFilelist\$ModuleName.psd1"
+$ModulePSM1file   = "$ModulePath\PSFilelist\$ModuleName.psm1"
+if (Get-Module -Name $ModuleName) 
+{
+  Remove-Module $ModuleName -Force 
+}
+Import-Module $ManifestPath -Verbose:$false
+
+Describe "Get-FLLogin" {
+    It "Outputs Hello World" {
+        Get-HelloWorld | Should be 'Hello World!'
+    }
+}
