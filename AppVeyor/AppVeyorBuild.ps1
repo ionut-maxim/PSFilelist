20627453da866d5d3dc7e9d99858c2e3343defc2
Ionuț Maxim <Ionu Maxim>
2017-01-24 12:41:34 +0200

    Fixed Write-Host errors

--- AppVeyor/AppVeyorBuild.ps1
+++ AppVeyor/AppVeyorBuild.ps1
@@ -1,10 +1,10 @@
-Write-Host 'Running AppVeyor build script' -ForegroundColor Yellow
-Write-Host 'ModuleName    : {0}' -f $env:ModuleName
-Write-Host 'Build version : {0}' -f $env:APPVEYOR_BUILD_VERSION
-Write-Host 'Author        : {0}' -f $env:APPVEYOR_REPO_COMMIT_AUTHOR
-Write-Host 'Branch        : {0}' -f $env:APPVEYOR_REPO_BRANCH
-Write-Host 'Repo          : {0}' -f $env:APPVEYOR_REPO_NAME
-Write-Host 'PSModulePath  :'
+Write-Host ('Running AppVeyor build script') -ForegroundColor Yellow
+Write-Host ('ModuleName    : {0}' -f $env:ModuleName)
+Write-Host ('Build version : {0}' -f $env:APPVEYOR_BUILD_VERSION)
+Write-Host ('Author        : {0}' -f $env:APPVEYOR_REPO_COMMIT_AUTHOR)
+Write-Host ('Branch        : {0}' -f $env:APPVEYOR_REPO_BRANCH)
+Write-Host ('Repo          : {0}' -f $env:APPVEYOR_REPO_NAME)
+Write-Host ('PSModulePath  :')
 
 $env:PSModulePath -split ';'
 
