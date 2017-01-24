c9d5535010f28a43e10444fd691bf72dce4458ef
Ionuț Maxim <Ionu Maxim>
2017-01-24 12:45:55 +0200

    Added HtmlAgilityPack Core To test the module on Powershell for Linux/MaCOS

--- PSFilelist/PSFilelist.psm1
+++ PSFilelist/PSFilelist.psm1
@@ -1,6 +1,6 @@
 #TODO 'Generate help with platyPS'
 $BaseUri = 'http://filelist.ro'
-Import-Module ('{0}\lib\HtmlAgilityPack.dll' -f $PSScriptRoot)
+Import-Module ('{0}\lib\HtmlAgilityPack.NetCore.dll' -f $PSScriptRoot)
 $ProgressPreference = 'SilentlyContinue'
 
 $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
